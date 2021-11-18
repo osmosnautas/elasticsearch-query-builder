a95ee85f5c6771419003ea0acae8b8049333e838
Fabian Castañeda <ing.fabiancm@gmail.com>
2021-11-18 13:50:53 -0600

    Add New class for handle multi_terms property in a query structure

---
+++ src/Query/MultiTermQuery.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Erichard\ElasticQueryBuilder\Query;
+
+class MultiTermQuery implements QueryInterface
+{
+    /** @var  */
+    private $field;
+
+    /** @var  */
+    private $terms;
+
+    /** @var  */
+    private $size;
+
+    /**
+     * @return mixed
+     */
+    public function getField()
+    {
+        return $this->field;
+    }
+
+    /**
+     * @param mixed $field
+     * @return MultiTermQuery
+     */
+    public function setField($field)
+    {
+        $this->field = $field;
+        array_push($this->terms, ['field' => $field]);
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTerms()
+    {
+        return $this->terms;
+    }
+
+    /**
+     * @param mixed $terms
+     * @return MultiTermQuery
+     */
+    public function setTerms($terms)
+    {
+        $this->terms = $terms;
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSize()
+    {
+        return $this->size;
+    }
+
+    /**
+     * @param mixed $size
+     * @return MultiTermQuery
+     */
+    public function setSize($size)
+    {
+        $this->size = $size;
+        return $this;
+    }
+
+    /**
+     * @return array
+     */
+    public function build(): array
+    {
+        $term = [
+            'field' => $this->field
+        ];
+
+        return [
+            'multi_terms' => [
+                'terms' => $this->terms,
+                'size' => $this->size
+            ]
+        ];
+    }
+}
